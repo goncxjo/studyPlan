377ec26c4dc4cecbf548be62e4565eb153fcafc5
Moreira <gonzalo-adrian.moreira@atos.net>
2018-08-14 09:19:04 -0300

    added username dropdown at navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Router } from '@angular/router';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,11 @@ export class AppComponent {
   title = 'studyPlan!';
   isNavbarCollapsed = true;
 
-  constructor(private db: AngularFireDatabase, public router: Router) { }
+  constructor(
+    private db: AngularFireDatabase
+    , public router: Router
+    , public auth: AuthService
+  ) { }
 
   isHome() {
     return this.router.url === '/';
